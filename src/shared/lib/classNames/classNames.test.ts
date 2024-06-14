0ba5aa4ee727044cc69d7e9bfb1db7776130a758
nurlan <anysoundspb@gmail.com>
2024-06-14 20:18:34 +0300

    add stylelint, eslint, jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,27 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('with first parameter', () => {
+    expect(classNames('someClass')).toBe('someClass');
+  });
+
+  test('with additional class', () => {
+    const expected = 'someClass additional';
+    expect(classNames('someClass', {}, ['additional'])).toBe(expected);
+  });
+
+  test('with mods class', () => {
+    const expected = 'someClass hovered scrollable additional';
+    expect(classNames('someClass', { hovered: true, scrollable: true }, ['additional'])).toBe(expected);
+  });
+
+  test('with mods false', () => {
+    const expected = 'someClass hovered additional';
+    expect(classNames('someClass', { hovered: true, scrollable: false }, ['additional'])).toBe(expected);
+  });
+
+  test('with mods undefined', () => {
+    const expected = 'someClass hovered additional';
+    expect(classNames('someClass', { hovered: true, scrollable: undefined }, ['additional'])).toBe(expected);
+  });
+});
