7961b18bb97d36221146a7164ddbcdbd9593c62c
nurlan <anysoundspb@gmail.com>
2024-06-22 22:08:13 +0300

    added error-boundary, page-loader, not-found-page

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,35 @@
+import React, { ReactNode, Suspense } from 'react';
+import { PageError } from 'widgets/PageError/ui/PageError';
+
+interface ErrorBoundaryProps {
+  children: ReactNode; // под ReactNode попадает любой react-компонент
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+    if (hasError) {
+      // You can render any custom fallback UI
+      return <Suspense fallback=""><PageError /></Suspense>;
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
