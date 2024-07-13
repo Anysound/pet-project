eb4722329b7cabbd3393a35f9994aed6c5f6f26f
nurlan <anysoundspb@gmail.com>
2024-07-13 16:10:18 +0300

    changed eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,7 +38,8 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
     'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] }],
-    'max-len': ['error', { ignoreComments: true, code: 110 }],
+    'max-len': ['error', { ignoreComments: true, code: 120 }],
+    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
   },
   globals: {
     __IS_DEV__: true,
