d0bf47ca47c2659ea025eca19536fac4a32e86f8
nurlan <anysoundspb@gmail.com>
2024-07-20 15:32:11 +0300

    some work on sidebar

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,15 +1,29 @@
 import { render, screen } from '@testing-library/react';
-import React from 'react';
-import { Button, ThemeButton } from './Button';
+
+
+import { Button, ButtonTheme } from './Button';
+
+
 
 describe('renders Button', () => {
+
   test('render Button', () => {
+
     render(<Button>Test</Button>);
+
     expect(screen.getByText('Test')).toBeInTheDocument();
+
   });
+
   test('render Button with Theme clear', () => {
-    render(<Button theme={ThemeButton.CLEAR}>Test</Button>);
+
+    render(<Button theme={ButtonTheme.CLEAR}>Test</Button>);
+
     expect(screen.getByText('Test')).toHaveClass('clear');
+
     screen.debug();
+
   });
+
 });
+
--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,22 +1,44 @@
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
+
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+
 import LightIcon from 'shared/assets/icons/theme-light.svg';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
+
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+
+
 
 interface ThemeSwitcherProps {
+
   className?: string;
+
 }
 
+
+
 export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
+
   const { theme, toggleTheme } = useTheme();
 
+
+
   return (
+
     <Button
-      theme={ThemeButton.CLEAR}
+
+      theme={ButtonTheme.CLEAR}
+
       onClick={toggleTheme}
+
       className={className}
+
     >
+
       {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
+
     </Button>
+
   );
+
 };
+
