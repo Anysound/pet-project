7e8de84918966e8fcfa793127c11703739b55806
Нурсултан Бергалиев <anysound@Noutbuk-Nursultan.local>
2024-08-04 15:20:04 +0300

    fixed variable in sb

--- src/shared/config/storybook/theme-decorator.tsx
+++ src/shared/config/storybook/theme-decorator.tsx
@@ -1,11 +1,14 @@
 import { Story } from '@storybook/react';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import { Theme, ThemeProvider } from 'app/providers/ThemeProvider';
 import 'app/styles/index.scss';
 
 export const ThemeDecorator = (theme: Theme) => (StoryJSX: Story) => (
-  <ThemeProvider initialTheme={theme}>
-    <div className={`app ${theme}`}>
-      <StoryJSX />
-    </div>
-  </ThemeProvider>
+  <StoreProvider>
+    <ThemeProvider initialTheme={theme}>
+      <div className={`app ${theme}`}>
+        <StoryJSX />
+      </div>
+    </ThemeProvider>
+  </StoreProvider>
 );
