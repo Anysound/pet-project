471841b202656754b3f0be9f04d2d176192d6d82
Нурсултан Бергалиев <anysoundspb@gmail.com>
2024-08-05 23:57:43 +0300

    fixed congig

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -1,6 +1,6 @@
 module.exports = {
   stories: [
-    '../../src/**/*.stories.@(js|jsx|ts|tsx)',
+    ['../../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   ],
   addons: [
     '@storybook/addon-links',
